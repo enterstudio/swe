0eab0c21375d07cadc1f9c116150907806ca362b
nhammond <nhammond@studynut>
2012-11-21 09:23:21 -0800

    Switch to Heroku db settings

--- settings.py
+++ settings.py
@@ -13,20 +13,20 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#DATABASES = {
-#    'default': dj_database_url.config()
-#    }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'swe',                      # Or path to database file if using sqlite3.
-        'USER': 'nhammond',                      # Not used with sqlite3.
-        'PASSWORD': 'nderf',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    'default': dj_database_url.config()
     }
-}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'swe',                      # Or path to database file if using sqlite3.
+#        'USER': 'nhammond',                      # Not used with sqlite3.
+#        'PASSWORD': 'nderf',                  # Not used with sqlite3.
+#        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
