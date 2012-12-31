a17fa15df2b48fdaba4b60b2457cdc30fa6ddd0c
Nathan Hammond <nathan.hammond@gmail.com>
2012-12-30 19:49:48 -0800

    change email signature to Managing Editor

--- swe/editorforms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-from swe.models import Project
-
-class ProjectForm(forms.ModelForm):
-    class Meta:
-        model = Project
